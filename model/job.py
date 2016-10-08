d65c24bc6a9bb335de698c758bf67e6a75b01fd5
WU Kai Chiu, ami <izayoi.ami@gmail.com>
2016-10-09 00:46:21 +0200

    Modify STA display

--- model/job.py
+++ model/job.py
@@ -34,8 +34,7 @@ class Job:
         self.executed = self.p.poll() is not None
         try:
             with open("{}.sta".format(self.name())) as f:
-                line = tailer.tail(f, 1)
-                line = line[0]
+                line = "".join(tailer.tail(f, 1))
             if line.find("NOT") == -1:
                 if line.find("COMPLETE") == 1:
                     self.state = "Succeed"
